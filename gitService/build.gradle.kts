5587af13def6aca5b1fa1d49e46838fc41fd5712
Constantine Suleimanov <skv26a@gmail.com>
2019-12-11 22:34:41 +0300

    init git service module

---
+++ gitService/build.gradle.kts
@@ -0,0 +1,28 @@
+
+plugins {
+	id("org.springframework.boot") version "2.2.2.RELEASE"
+	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	java
+	idea
+	war
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+}
+
